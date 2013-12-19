7fb5d3c2bb512b9d4d697229aec9c0c4c6919f92
zc238 <zc238@cornell.edu>
2013-12-18 20:56:04 -0800

    Initial Commit

--- VenueLiquidity/VenueLiquidity/AdverseSelection.cpp
+++ VenueLiquidity/VenueLiquidity/AdverseSelection.cpp
@@ -5,14 +5,45 @@
 using namespace std;
 using namespace H5;
 
+typedef struct exegyRow {
+	float ask;
+	char ask_exchange[4];
+	int ask_size;
+	float bid;
+	char bid_exchange[4];
+	int bid_size;
+	char exchange[4];
+	int exchange_time;
+	int instrument_status;
+	int latency; 
+	int line;
+	int market_status;
+	float prev_close;
+	float price;
+	int quals;
+	char refresh[4];
+	int seq_no;
+	int size;
+	char sub_market[4];
+	char symbol[4];
+	int thru_exempt;
+	char time[4];
+	char type[4];
+	int volume;
+} exegyRow;
+
 void AdverseSelection::parseHdf5Source()
 {
 	H5File file2(hdf5Source, H5F_ACC_RDONLY);
 	
 	DataSet dataset = file2.openDataSet("/ticks/AMZN");
-
-	/*DataSpace space = dataset.getSpace();
-	space.selectAll();*/
+	
+	size_t size = dataset.getInMemDataSize();
+	exegyRow *s = (exegyRow*) malloc(size);
+	CompType type = dataset.getCompType();
+	cout<<type.getNmembers()<<endl;
+	dataset.read(s, type);
+	exegyRow r = s[1];
 }
 
 AdverseSelection::AdverseSelection(H5std_string source)
