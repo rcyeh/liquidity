648ab98747ebf4d6409790b30d118a0b3e880438
zc238 <zc238@cornell.edu>
2014-01-06 17:22:12 -0800

    re-uploaded results.

--- VenueLiquidity/VenueLiquidity/Runner.cpp
+++ VenueLiquidity/VenueLiquidity/Runner.cpp
@@ -13,10 +13,10 @@ int main(int argc, char * argv[]){
 	//First populate all stock list
 	AdverseSelection::populateStockLists(file);
 	//cout<<AdverseSelection::allStocks.at(15)<<endl;
-	for (int i=10; i<AdverseSelection::allStocks.size(); ++i){
+	for (int i=0; i<AdverseSelection::allStocks.size(); ++i){
 		cout<<i<<endl;
-		//AdverseSelection selection(file, AdverseSelection::allStocks.at(i));
-		AdverseSelection selection(file, "ABW_hyphen_B");
+		AdverseSelection selection(file, AdverseSelection::allStocks.at(i));
+		//AdverseSelection selection(file, "ABW_hyphen_B");
 		selection.outputAdvSelToFile();
 	}
 }
